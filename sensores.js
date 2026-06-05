f1dbd6ef0c4fbe445a8ce4c78f50321dc25c31ba
Rafael Manzan <rafa.a.manzan@gmail.com>
2026-06-05 13:21:09 -0300

    Fix sensores function to use 'numeros' array

--- sensores.js
+++ sensores.js
@@ -3,17 +3,18 @@ function sensores (numeros) {
     let soma = 0    
     let media = 0    
     let resfriamento    
-    for (let i = 0; i < temperatura.length; i++){        
-        soma += temperatura[i]    
-    }    
-    media = soma / temperatura.length    
+    for (let i = 0; i < numeros.length; i++) {
+        soma += numeros[i];
+    }
+    
+    media = soma / numeros.length;    
     
     if (media >= 155){        
-        console.log("Resfriamento ativo")        
+        console.log("Resfriamento ativo, média: ", media)        
         return resfriamento = true    
     }    
     else {        
-        console.log("Resfriamento inativo")        
+        console.log("Resfriamento inativo, média: ", media)        
         return resfriamento = false    
     }
 }
